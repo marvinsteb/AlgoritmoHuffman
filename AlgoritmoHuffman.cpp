03b9918e8e80b4c501873bf06b0cf7811e9a6d87
Marvin Menchú <mmenchu@outlook.com>
2019-05-23 10:57:59 -0600

    agregado arbol de huffman, para convertir caracteres a sus correspondientes binarios

--- AlgoritmoHuffman.cpp
+++ AlgoritmoHuffman.cpp
@@ -4,23 +4,52 @@
 
 using namespace std;
 
-
-
 /*  constantes */
-#define LONJITUDMAXarchivo 100
-#define ALTURAMAXARBOL 100
+#define LONJITUDMAXARCHIVO 100
+#define ALTURAARBOL 100
 
 
-/* funciones */
+/*  prototipos de funciones */
 void agregarCaracter(char);
-void agregarCaracterAlArreglo(char,int);	
+void agregarCaracterAlArreglo(char,int);
+void arbolDIntercambio(struct Nodo **, struct Nodo **);
+void codigicarArreglo(char[], int[], int);	
 
 /* variables */
-char caracteres[LONJITUDMAXarchivo];
-int frecuencias[LONJITUDMAXarchivo];
+char caracteres[LONJITUDMAXARCHIVO];
+int frecuencias[LONJITUDMAXARCHIVO];
+int resultado[LONJITUDMAXARCHIVO];
 int indiceCaracteres = 0; 
+int indiceResultado = 0;
 
-/*  estructuras */
+/* estructuras para el arbol huffman */
+struct Nodo{
+    char caracter;
+    unsigned frecuencia;
+    struct Nodo *izquierda, *derecha;
+};
+struct NodoRaiz{
+    unsigned altura;
+    unsigned capacidad;
+    struct Nodo **array;
+};
+
+struct Nodo *nuevoNodo(char caracter, unsigned frecuencia){
+    struct Nodo *temporal = (struct Nodo *)malloc(sizeof(struct Nodo));
+    temporal->izquierda = temporal->derecha = NULL;
+    temporal->caracter = caracter;
+    temporal->frecuencia = frecuencia;
+    return temporal;
+}
+
+struct NodoRaiz *crearNodoRaiz(unsigned capacidad){
+
+    struct NodoRaiz *ndRaiz = (struct NodoRaiz *)malloc(sizeof(struct NodoRaiz));
+    ndRaiz->altura = 0;
+    ndRaiz->capacidad = capacidad;
+    ndRaiz->array = (struct Nodo **)malloc(ndRaiz->capacidad * sizeof(struct Nodo *));
+    return ndRaiz;
+}
 
 int main(int argc, char** argv) {
       printf("\n\t\tAlgoritmo de huffman \n\n");	
@@ -38,13 +67,14 @@ int main(int argc, char** argv) {
       for (int indice = 0; indice < indiceCaracteres; indice++){
                printf("%c - %i \n",caracteres[indice],frecuencias[indice]);
       }
-      
-    cout<<"\n\n";
-    system("pause");
-    return 0;
+       
+      codigicarArreglo(caracteres,frecuencias,indiceCaracteres);
+      printf("\n\n");
+      system("pause");
+      return 0;
 
 }
-
+/*  cuenta las frecuencias de cada caracter del archivo */
 
 void agregarCaracter(char caracter){
       int bandera = 0;
@@ -64,5 +94,166 @@ void agregarCaracter(char caracter){
          indiceCaracteres++; 
          printf("agregando caracter %c \n",caracter);
       } 
+}
+
+
+void arbolDIntercambio(struct Nodo **a, struct Nodo **b){
+    struct Nodo *t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void crearArbolInicial(struct NodoRaiz *ndRaiz, int indice){
+    int alturaInicial = indice;
+    int izquierda = 2 * indice + 1;
+    int derecha = 2 * indice + 2;
+
+    if (izquierda < ndRaiz->altura && ndRaiz->array[izquierda]->frecuencia < ndRaiz->array[alturaInicial]->frecuencia)
+        alturaInicial = izquierda;
+
+    if (derecha < ndRaiz->altura && ndRaiz->array[derecha]->frecuencia < ndRaiz->array[alturaInicial]->frecuencia)
+        alturaInicial = derecha;
+
+    if (alturaInicial != indice){
+        arbolDIntercambio(&ndRaiz->array[alturaInicial], &ndRaiz->array[indice]);
+        crearArbolInicial(ndRaiz, alturaInicial);
+    }
+}
+
+int AlturaInicial(struct NodoRaiz *ndRaiz){
+    return (ndRaiz->altura == 1);
+}
+
+struct Nodo *extrarValorMinimo(struct NodoRaiz *ndRaiz){
+    struct Nodo *temporal = ndRaiz->array[0];
+    ndRaiz->array[0] = ndRaiz->array[ndRaiz->altura - 1];
+
+    --ndRaiz->altura;
+    crearArbolInicial(ndRaiz, 0);
+
+    return temporal;
+}
+
+void incertarNodoRaiz(struct NodoRaiz *ndRaiz, struct Nodo *nodoInicio){
+    ++ndRaiz->altura;
+    int indice = ndRaiz->altura - 1;
+
+    while (indice && nodoInicio->frecuencia < ndRaiz->array[(indice - 1) / 2]->frecuencia){
+        ndRaiz->array[indice] = ndRaiz->array[(indice - 1) / 2];
+        indice = (indice - 1) / 2;
+    }
+
+    ndRaiz->array[indice] = nodoInicio;
+}
+
+void costruirNodoArbol(struct NodoRaiz *ndRaiz){
+    int n = ndRaiz->altura - 1;
+    int indice;
+    for (indice = (n - 1) / 2; indice >= 0; --indice)
+        crearArbolInicial(ndRaiz, indice);
+}
+
+void imprimeAregloCodificado(int arregloCaracteres[], int n){
+    int indice;
+    for (indice = 0; indice < n; ++indice)
+        printf("%i", arregloCaracteres[indice]);
+
+    printf( "\n");
+}
+
+
+void guardarArchivoCodificado(int arregloCaracteres[], int altura){
+    char caracter;    
+     ifstream archivo("archivo2.txt");  
+      while (archivo.get(caracter)){
+         resultado[indiceResultado] = caracter;
+         indiceResultado++;
+      }        
+     archivo.close();
+	        
+           for (int indice = 0; indice < altura; ++indice){
+            resultado[indiceResultado] = arregloCaracteres[indice];
+            indiceResultado++;
+      } 
+
+}
+
+int esHoja(struct Nodo *raiz){
+    return !(raiz->izquierda) && !(raiz->derecha);
+}
+
+struct NodoRaiz *createAndBuildMinHeap(char caracter[], int frecuencia[], int altura){
+    struct NodoRaiz *ndRaiz = crearNodoRaiz(altura);
+
+    for (int indice = 0; indice < altura; ++indice)
+        ndRaiz->array[indice] = nuevoNodo(caracter[indice], frecuencia[indice]);
+
+    ndRaiz->altura = altura;
+    costruirNodoArbol(ndRaiz);
+
+    return ndRaiz;
+}
+
+struct Nodo *costruirArbolHufman(char caracter[], int frecuencia[], int altura){
+    struct Nodo *izquierda, *derecha, *inicio;
+
+    struct NodoRaiz *ndRaiz = createAndBuildMinHeap(caracter, frecuencia, altura);
+
+    while (!AlturaInicial(ndRaiz)) {
+
+        izquierda = extrarValorMinimo(ndRaiz);
+        derecha = extrarValorMinimo(ndRaiz);
+
+        inicio = nuevoNodo('$', izquierda->frecuencia + derecha->frecuencia);
+
+        inicio->izquierda = izquierda;
+        inicio->derecha = derecha;
+
+        incertarNodoRaiz(ndRaiz, inicio);
+    }
+
+    return extrarValorMinimo(ndRaiz);
+}
+
+void imprimeCaracteres(struct Nodo *raiz, int arregloCaracteres[], int inicio){
+
+    if (raiz->izquierda){
+        arregloCaracteres[inicio] = 0;
+        imprimeCaracteres(raiz->izquierda, arregloCaracteres, inicio + 1);
+    }
+
+    if (raiz->derecha){
+        arregloCaracteres[inicio] = 1;
+        imprimeCaracteres(raiz->derecha, arregloCaracteres, inicio + 1);
+    }
+
+    if (esHoja(raiz)){
+        cout << raiz->caracter << ": ";
+        imprimeAregloCodificado(arregloCaracteres, inicio);
+        guardarArchivoCodificado(arregloCaracteres, inicio);
+    }
+}
+
+void codigicarArreglo(char caracter[], int frecuencia[], int altura){
+    struct Nodo *raiz = costruirArbolHufman(caracter, frecuencia, altura);
+
+    int arregloCaracteres[ALTURAARBOL], inicio = 0;
+
+    imprimeCaracteres(raiz, arregloCaracteres, inicio);
+
+    printf("\n resultado del archivo");
+
+     ofstream archivo2("archivo2.txt");
+      
+    if(archivo2.fail()){
+    	printf("error al crear el archivo \n");
+    } else {
+
+       for (int indice = 0; indice < indiceResultado; ++indice){
+        printf("%i",resultado[indice]);   
+        archivo2 << resultado[indice];
+      }
+	archivo2.close();
+    }
 }
 
